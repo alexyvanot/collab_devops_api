b9423361006c09795d7bcab706625c5d83575774
VANOT Alexy <alexystefen@hotmail.com>
2024-11-21 12:43:27 +0100

    init

---
+++ src/main/java/fr/alexyvanot/collab_devops_api/CollabDevopsApiApplication.java
@@ -0,0 +1,13 @@
+package fr.alexyvanot.collab_devops_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabDevopsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollabDevopsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/alexyvanot/collab_devops_api/CollabDevopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alexyvanot.collab_devops_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabDevopsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
